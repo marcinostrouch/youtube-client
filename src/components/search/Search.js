ee8f8328b7a379028ee7b948bebc7fd91d7140fd
Marcin Ostrouch <marcin.ostrouch@monogo.pl>
2021-09-16 06:08:52 +0200

    Add API check in Search

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
+import axios from "axios";
 import * as searchStyles from "./search.module.scss";
 
 export const Search = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleInputChange = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSubmit = e => {
     e.preventDefault();
 
-    const searchTxt = e.target.value;
-    console.log("searchTxt ===", searchTxt);
-    setSearchTerm(searchTxt);
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&type=channel&q=${searchTerm}&safeSearch=none&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
+      )
+      .then(response => {
+        console.log(response.data.items);
+      });
   };
 
   return (
     <div className={searchStyles.searchContainer}>
-      <input
-        type="text"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={e => handleInputChange(e)}
-      />
+      <form onSubmit={e => handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={e => handleInputChange(e)}
+        />
+        <button type="submit" aria-label="Search">
+          SearchBtnIcon
+        </button>
+      </form>
     </div>
   );
 };
