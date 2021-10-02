f07672da5051396eb89354f8847319f1866b9342
Marcin Ostrouch <marcin.ostrouch@monogo.pl>
2021-10-02 07:36:54 +0200

    Fix vid player width responsiveness issue

--- src/components/videoPlayer/VideoPlayer.js
+++ src/components/videoPlayer/VideoPlayer.js
@@ -1,41 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import ReactPlayer from "react-player";
 import * as videoPlayerStyles from "./videoPlayer.module.scss";
 
 export const VideoPlayer = ({ videoId }) => {
-  const [windowWidth, setWindowWidth] = useState(0);
-  const [playerWidth, setPlayerWidth] = useState(0);
-
   const videoUrl = `https://www.youtube.com/watch?v=${
     videoId || "6JQm5aSjX6g"
   }} `;
 
-  const updateDimensions = () => {
-    const x = window.innerWidth;
-    setWindowWidth(x);
-  };
-
-  useEffect(() => {
-    updateDimensions();
-
-    window.addEventListener("resize", updateDimensions);
-
-    return () => window.removeEventListener("resize", updateDimensions);
-  }, []);
-
-  useEffect(() => {
-    if (windowWidth > 992) {
-      setPlayerWidth(windowWidth - 300);
-    } else {
-      setPlayerWidth(windowWidth);
-    }
-  }, [windowWidth]);
-
   return (
-    <div className={videoPlayerStyles.videoPlayer}>
+    <div className={videoPlayerStyles.playerWrapper}>
       <ReactPlayer
-        height={`${(playerWidth * 9) / 16}px`}
-        width={`${playerWidth}px`}
+        className={videoPlayerStyles.reactPlayer}
+        height="100%"
+        width="100%"
         url={videoUrl}
         config={{ youtube: { playerVars: { autoplay: 1 } } }}
         controls
