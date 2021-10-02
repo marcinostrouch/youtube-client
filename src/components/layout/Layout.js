83a5eaa9a50061fd52497bf26ec8abf8342e8002
Marcin Ostrouch <marcin.ostrouch@monogo.pl>
2021-10-02 09:32:48 +0200

    Improve videoItem info styling

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Header } from "../header/Header";
-import { Footer } from "../footer/Footer";
 import * as layoutStyles from "./layout.module.scss";
 
 const Layout = ({ children }) => (
   <>
     <Header />
     <main className={layoutStyles.mainContainer}>{children}</main>
-    <Footer />
   </>
 );
 
--- src/components/videoListItem/VideoListItem.js
+++ src/components/videoListItem/VideoListItem.js
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import { selectVideo } from "../../redux/selectedVideo";
 import * as videoListItemStyles from "./videoListItem.module.scss";
 
+// TODO: convert timestamp to a  "time from now" value
+
 export const VideoListItem = ({ videoItem }) => {
   const { channelTitle, viewCount, timeStamp, title, imageUrl } = videoItem;
 
@@ -15,20 +17,22 @@ export const VideoListItem = ({ videoItem }) => {
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/interactive-supports-focus
     <div
-      className={videoListItemStyles.videoListItem}
+      className={videoListItemStyles.item}
       role="button"
       onClick={() => handleClick(videoItem)}
     >
-      <button type="button">
-        <img src={imageUrl} alt="" />
-      </button>
-      <div className="video-row__text">
-        <button type="button">
-          <h3>{title}</h3>
-          <p>
-            {channelTitle} * {viewCount} views * {timeStamp}
-          </p>
-        </button>
+      <img src={imageUrl} alt="" />
+
+      <div className={videoListItemStyles.metaData}>
+        <h3>{title}</h3>
+        <div className={videoListItemStyles.secondaryMetaData}>
+          <div className={videoListItemStyles.channelName}>{channelTitle}</div>
+
+          <span className={videoListItemStyles.viewCount}>
+            {viewCount} views
+          </span>
+          <span>{timeStamp}</span>
+        </div>
       </div>
     </div>
   );
